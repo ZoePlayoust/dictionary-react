4ced73e1510011dcc5186189158cec29386f3ac9
ZoePlayoust <playoustz@gmail.com>
2022-05-11 11:13:12 +0200

    Creating file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App mt-5 ">
+      <h1>Dictionary</h1>
+      <div className="row">
+        <div className="col-6">Hello</div>
+        <div className="col-6">Hello</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
